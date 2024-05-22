5585f4834b6a16118c3f704c54285384b6029138
unknown <elhadjoumartall10@gmail.com>
2024-05-22 09:39:17 +0200

    modification du couleur de l'écriture

--- lib/widgets/common/appbar_widget.dart
+++ lib/widgets/common/appbar_widget.dart
@@ -6,7 +6,13 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: const Text('Store'),
+      title: const Text(
+        'Store',
+        style: TextStyle(
+          // color: colors.deepOrange,
+          color: Color.fromRGBO(255, 0, 255, 1),
+        ),
+      ),
       centerTitle: true,
     );
   }
