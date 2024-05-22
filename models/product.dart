e4678f30353e779bca18f16083da83cb6154417c
unknown <elhadjoumartall10@gmail.com>
2024-05-22 16:10:53 +0200

    Creation des identification de l'API

--- models/product.dart
+++ models/product.dart
@@ -1,9 +1,24 @@
-class Product{
+class Product {
   // Creer le constructeur
   /*
   {} : paramètre nommé
   this : paramètre promu pour le constructeur
   required : paramètre obligatoire
   */
-  Product({})
+  int id;
+  String name;
+  String title, description, category, image;
+  num price;
+  Map rating;
+
+  // Creer le constructeur par defaut
+  Product(
+      {required this.id,
+      required this.name,
+      required this.title,
+      required this.description,
+      required this.category,
+      required this.image,
+      required this.price,
+      required this.rating});
 }
