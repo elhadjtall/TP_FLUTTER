77d65ab72227fde606f0282b9ffd64f8847f4475
unknown <elhadjoumartall10@gmail.com>
2024-05-22 15:32:24 +0200

    Creation du fichier pour l'API

---
+++ models/product.dart
@@ -0,0 +1,9 @@
+class Product{
+  // Creer le constructeur
+  /*
+  {} : paramètre nommé
+  this : paramètre promu pour le constructeur
+  required : paramètre obligatoire
+  */
+  Product({})
+}
